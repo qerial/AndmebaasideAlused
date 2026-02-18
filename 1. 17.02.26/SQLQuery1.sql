2cdc50a39742ac13620aa562bbcb22eefd306ac3
Opilane TTHK <opilane@tthk.ee>
2026-02-18 11:35:20 +0200

    hhh

--- 1. 17.02.26/SQLQuery1.sql
+++ 1. 17.02.26/SQLQuery1.sql
@@ -22,23 +22,16 @@ id int not null primary key,
 -- ei tohi olla tühi
 Gender nvarchar(10) not null
 )
-
-
-
-
-
 --andmete sisestamine
 --proovige ise teha
 --Id 1, Gender Male
 --Id 2, Gender Female
-
 INSERT INTO Gender(id, Gender)
 VALUES(1, 'Male'),
 (2, 'Female')
-
 --vaatame tabeli sisu
 --* Tähendab, et näita kõike seal sees olevat infot
-select * from Gender
+select * from Person
 
 --teeme tabeli nimega Person
 --Veeru nimed: Id int not null primary key,
@@ -46,13 +39,110 @@ select * from Gender
 --Email nvarchar (30)
 --Genderid int
 
+
 create table Person		
 (
 id int not null primary key,
+Age nvarchar(10),
 Name nvarchar(30),
 Email nvarchar(30),
 GenderId int
 )
 
+insert into Person (Id, Name, Email, GenderId)
+values (1, 'Superman', 's@s.com', 2),
+(2, 'Wonderwoman', 'w@w.com', 1),
+(3, 'Batman', 'b@b.com', 2), 
+(4, 'Aquaman', 'a@a.com', 2), 
+(5, 'Catwoman', 'c@c.com', 1), 
+(6, 'Antman', 'ant "ant.com', 2), 
+(8, NULL, NULL, 2)
+
+--näen tabelis olevat infot
+--võõrvõtme ühenduse loomine kahe tabeli vahel
+alter table Person add constraint tblPerson_GenderId_FK
+foreign key (GenderId) references Gender(Id)
 
 
+-- kui sisestad uue rea andmeid ja ei ole sisestanud GenderId alla
+--väärtust, siis automaatselt sisestab sellele reale väärtuse 3
+--e unknown
+alter table Person
+add constraint DF_Persons_GenderId
+Default 3 for genderId
+
+Insert into Gender (Id, Gender)
+values (3, 'Unknown')
+
+insert into Person (Id, Name, Email)
+values (9, 'Spiderman', 'spider@man.com')
+
+select * from Person
+
+--piirangu kustutamine
+alter table Person 
+drop constraint DF_Persons_GenderId
+
+--kuidas lisada veerrgu tabelile Person
+--veeru nimi on Age nvarchar
+
+alter table Person 
+add Age nvarchar(10)
+
+alter table Person
+add constraint CK_Person_Age check (Age > 0 and Age < 155)
+
+--kuidas uuendada andmeid
+update Person
+set Age = 159
+where Id = 6
+
+select * from Person
+
+--soovin kustutada ühe rea
+--????
+
+delete from Person
+where Id = 8
+
+
+--lisame uue veeru City nvarchar(50)
+
+alter table Person 
+add City nvarchar(50)
+
+--kõik, kes elavad Gothami linnas
+select * from Person where city = 'Gotham city'
+--kõik kes ei ela gothamis
+select * from Person where City != 'Gotham city'
+--variant nr 2. kõik, kes ei ela Gothamis
+select * from Person where City <> 'Gotham'
+
+--näitab teatud vanusega inimesi
+--valime 120, 35 ja 25
+select * from Person where Age = 120 or Age = 35 or Age = 26
+select * from Person Where Age IN (120, 26, 35);
+
+--soovin näha inimesi vahemikus 22 kuni 41
+
+select * from Person where (Age > 21 and Age < 42)
+
+--wildcard e näitab kõik g-tähega linnad
+select * from Person where City Like 'g%' ;
+--otsib emailid @-märgiga
+select * from Person where Email like '%@%'
+
+--tahan näha, kellel on emailis ees ja peale @-märki üks täht
+select * from Person where Email like '%_@_.com%'
+
+--kõik, kelle nimes ei ole esimene täht W, A, S
+
+select * from Person where Name like '[^^WAS]%'
+
+--kõik, kes elavad Gothamis ja New Yorkis
+
+select * from Person where (City = 'Gotham city' or City = 'New York')
+
+--kõik, kes elavad Gothamis ja New Yorkis ning peavad olema 
+--vanemad kui 29
+select * from Person where (City = 'Gotham city' or City = 'New York' and Age > 29)
